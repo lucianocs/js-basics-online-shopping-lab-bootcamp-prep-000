c974aa8a5e4a6d0640936990c84b4b5fe283cf60
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-25 22:44:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,21 @@ function setCart(c) {
   return cart;
 }
 
+
+/*
+1. The `addToCart()` function accepts one argument, the name of an item.
+    + Use that passed-in string to create a new object representing the item. The object should consist of one key-value pair in which the key is the item's name and the value is a randomly-generated price: `{ itemName: itemPrice }`. As more items are added, the `cart` should start to look something like this: `[ { "bananas": 17 }, { "pancake batter": 5 }, { "eggs": 49 }]`.
+    + The price of each item should be a randomly-generated integer between 1 and 100.
+      * ***HINT***: Look into `Math.random()` and `Math.floor()`.
+    + If you're struggling to create a new object from the item name, remember that ES6 provides a nifty way to dynamically set the keys in an object literal. That is, it allows the use of variables as keys. If you're having trouble recalling the syntax, take a look back at the **Top Tip** in the [JavaScript Objects](https://learn.co/lessons/javascript-objects) lesson.
+    + Upon the successful addition of a new item to the cart, the function should print `<itemName> has been added to your cart.` to the console and then `return` the updated `cart`.
+
+*/
+
 function addToCart(item) {
-  var o = new Object(item);
+  var price =  Math.random() * (100 - 1) + 1;
+  
+  var obj = new Object({ item:  })
 
   if(cart.length = 0){
     cart[0] = item
