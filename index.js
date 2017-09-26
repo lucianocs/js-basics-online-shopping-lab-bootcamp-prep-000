fd67825008cc3521dbd14decc7546706dfe4e05e
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-26 00:05:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function setCart(c) {
 
 
 /*
-2. The `viewCart()` function does not accept any arguments. 
-It should loop over every item in your cart, printing out the contents as one long, 
+2. The `viewCart()` function does not accept any arguments.
+It should loop over every item in your cart, printing out the contents as one long,
 coherent statement in this format: `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
     + If the cart is empty, the function should instead print out `Your shopping cart is empty.`
-    + ***Note***: Pay close attention to the syntax above. The printed statement 
+    + ***Note***: Pay close attention to the syntax above. The printed statement
     should be a single sentence that begins with `In your cart, you have `,
      terminates in a period, and can assume the following shapes according to how many items the cart contains:
       * 1 item — `In your cart, you have bananas at $17.`
@@ -38,7 +38,9 @@ function addToCart(itemName) {
 }
 
 function viewCart() {
-return 'Your shopping cart is empty.'
+  if (cart.length == 0) {
+    return 'Your shopping cart is empty.'
+  } 
 }
 
 function total() {
