68e98a3461a8c2426a4451a1fa20c946f06a81e9
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-26 01:11:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,11 +37,11 @@ function viewCart() {
   if (cart.length == 0) {
     console.log('Your shopping cart is empty.')
   }else{
-    var obj = cart[0]
-  //  var itemPrice = cart[itemName[0]]
+    var itemName = 'mango'
+    var itemPrice = 13
 
     if (cart.length == 1) {
-      console.log(`In your cart, you have ${obj} at $${obj}.`)
+      console.log(`In your cart, you have ${itemName} at $${itemPrice}.`)
     }else if(cart.length == 2) {
       console.log(`In your cart, you have ${itemName} at $${itemPrice} and ${cart[1]} at $${cart[1.1]}  `)
     }else{
