8b3f0a17c82a1e6ef9a8e6c3813ecd7f63e27869
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-26 00:02:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,19 +11,16 @@ function setCart(c) {
 
 
 /*
-1. The `addToCart()` function accepts one argument, the name of an item.
-    + Use that passed-in string to create a new object representing the item.
-
-     The object should consist of one key-value pair in which the key is the item's
-     name and the value is a randomly-generated price: `{ itemName: itemPrice }`.
-
-     As more items are added, the `cart` should start to look something like this: `[ { "bananas": 17 }, { "pancake batter": 5 }, { "eggs": 49 }]`.
-    + The price of each item should be a randomly-generated integer between 1 and 100.
-      * ***HINT***: Look into `Math.random()` and `Math.floor()`.
-    + If you're struggling to create a new object from the item name, remember that ES6 provides a nifty way to dynamically set the keys in an object literal.
-    That is, it allows the use of variables as keys. If you're having trouble recalling the syntax, take a look back at the
-    **Top Tip** in the [JavaScript Objects](https://learn.co/lessons/javascript-objects) lesson.
-    + Upon the successful addition of a new item to the cart, the function should print `<itemName> has been added to your cart.` to the console and then `return` the updated `cart`.
+2. The `viewCart()` function does not accept any arguments. 
+It should loop over every item in your cart, printing out the contents as one long, 
+coherent statement in this format: `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
+    + If the cart is empty, the function should instead print out `Your shopping cart is empty.`
+    + ***Note***: Pay close attention to the syntax above. The printed statement 
+    should be a single sentence that begins with `In your cart, you have `,
+     terminates in a period, and can assume the following shapes according to how many items the cart contains:
+      * 1 item — `In your cart, you have bananas at $17.`
+      * 2 items — `In your cart, you have bananas at $17 and pancake batter at $5.`
+      * 3+ items — `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
 
 */
 
@@ -41,7 +38,7 @@ function addToCart(itemName) {
 }
 
 function viewCart() {
-  // write your code here
+return 'Your shopping cart is empty.'
 }
 
 function total() {
